565dd4a8aee813abfb1ef761a7faed9814d53c3a
Rodrigo Werlang <rodrigo@paradigmabs.com.br>
2024-05-27 13:57:51 -0300

    include command to update vmss deployment

--- images.CI/linux-and-win/deploy-vmss.ps1
+++ images.CI/linux-and-win/deploy-vmss.ps1
@@ -12,7 +12,6 @@ param (
     [string] $AdminSecretName,
     [int] $DiskSizeGb = 0,
     [string] $StorageType = "StandardSSD_LRS",
-    [string] $SshPublicKey,
     [string] $AzureSshKey,
     [string] $AzureSshKeyResourceGroup = $ResourceGroupName
 )
@@ -28,6 +27,16 @@ function CheckCommandResult {
 
 $conditionalParameters = @()
 $customScriptParameters = @()
+$tempPath = $env:AGENT_TEMPDIRECTORY
+
+if (!$tempPath) { $tempPath = $env:TEMP }
+if (!$tempPath) { $tempPath = "./" }
+
+if ($IsWindows) {
+    $conditionalParameters += "--load-balancer='`"`"'"
+} else {
+    $conditionalParameters += "--load-balancer=`"`""
+}
 
 if ($ImageType.StartsWith("windows")) {
     if ($DiskSizeGb -eq 0) { $DiskSizeGb = 256 }
@@ -37,7 +46,7 @@ if ($ImageType.StartsWith("windows")) {
     } elseif ($KeyVault -and $AdminSecretName) {
         $pwValue = $(az keyvault secret show --name $AdminSecretName --vault-name $KeyVault --query value -o tsv)
     } else {
-        Write-Error "Required missing parameters for Windows VM. AdminPassword parameter is empty. Alternatives KeyVault with AdminSecretName parameters are also empty"
+        Write-Error "Missing required parameters for Windows VM. AdminPassword parameter is empty. Alternatives KeyVault with AdminSecretName parameters are also empty"
         exit 1
     }
     
@@ -53,15 +62,20 @@ if ($ImageType.StartsWith("windows")) {
 } else {
     if ($DiskSizeGb -eq 0) { $DiskSizeGb = 128 }
 
-    if (!$SshPublicKey -and $AzureSshKey) {
+    if ($AzureSshKey) {
         Write-Host "Retrieving ssh key from Azure ssh key ..."
 
-        $SshPublicKey = $(az sshkey show --name $AzureSshKey --resource-group $AzureSshKeyResourceGroup --query publicKey -o tsv)
+        $sshPublicKey = $(az sshkey show --name $AzureSshKey --resource-group $AzureSshKeyResourceGroup --query publicKey -o tsv)
         CheckCommandResult
+
+        $sshFile = Join-Path -Path $tempPath -ChildPath "ssh.pub"
+        $sshFile
+        Set-Content -Path $sshFile -Value $sshPublicKey -Force
+
+        $conditionalParameters += "--ssh-key-values=$sshFile"
     }
 
     $conditionalParameters += "--authentication-type=ssh"
-    $conditionalParameters += "--ssh-key-values=`"$SshPublicKey`""
 
     $commandToExecute = "sudo su -c \\\`"find /opt/post-generation -mindepth 1 -maxdepth 1 -type f -name '*.sh' -exec bash {} \\;\\\`""
     $customScriptParameters += "--name=CustomScript"
@@ -70,43 +84,50 @@ if ($ImageType.StartsWith("windows")) {
     $customScriptParameters += "--settings=`"{\`"commandToExecute\`":\`"$commandToExecute\`" }`""
 }
 
-Write-Host "Conditional parameters:"
-Write-Host $conditionalParameters
-
-Write-Host ""
-Write-Host "Custom script parameters:"
-Write-Host $customScriptParameters
-Write-Host ""
-
 Write-Host "Deploy Azure VMSS $VmssName ..."
 Write-Host "VM sku: $VmSku"
 Write-Host "OS type: $ImageType"
 Write-Host "Image: $Image"
 Write-Host "Disk size: $DiskSizeGb"
 Write-Host "Disk type: $StorageType"
-
-az vmss create --name $VmssName `
-    --resource-group $ResourceGroupName `
-    --image $Image `
-    --vm-sku $VmSku `
-    --instance-count 0 `
-    --storage-sku $StorageType `
-    --os-disk-size-gb $diskSizeGb `
-    --encryption-at-host `
-    --admin-username $AdminUserName `
-    --assign-identity "[system]" `
-    --disable-overprovision `
-    --enable-auto-update false `
-    --subnet $SubnetId `
-    --upgrade-policy-mode manual `
-    --single-placement-group false `
-    --platform-fault-domain-count 1 `
-    --load-balancer '""' `
-    --orchestration-mode Uniform `
-    --only-show-errors `
-    $conditionalParameters
-
 Write-Host ""
+
+Write-Host "Verify if VMSS '$VmssName' already exists ..."
+$vmss = $(az vmss list --resource-group $ResourceGroupName --query "[?name=='$VmssName']") | ConvertFrom-Json -Depth 20
+
+if ($vmss.Length -eq 0) {
+    Write-Host "Resource doesn't exit. Creating ..."
+
+    az vmss create --name $VmssName `
+        --resource-group $ResourceGroupName `
+        --image $Image `
+        --vm-sku $VmSku `
+        --instance-count 0 `
+        --storage-sku $StorageType `
+        --os-disk-size-gb $diskSizeGb `
+        --encryption-at-host `
+        --admin-username $AdminUserName `
+        --assign-identity "[system]" `
+        --disable-overprovision `
+        --enable-auto-update false `
+        --subnet $SubnetId `
+        --upgrade-policy-mode manual `
+        --single-placement-group false `
+        --platform-fault-domain-count 1 `
+        --orchestration-mode Uniform `
+        --only-show-errors `
+        $conditionalParameters
+
+    Write-Host ""
+
+} else {
+    Write-Host "Resource already exists. Updating ..."
+
+    az vmss update --name $VmssName `
+        --resource-group $ResourceGroupName `
+        --set virtualMachineProfile.storageProfile.imageReference.id=$Image
+}
+
 CheckCommandResult
 
 Write-Host "Add custom script extension ..."
