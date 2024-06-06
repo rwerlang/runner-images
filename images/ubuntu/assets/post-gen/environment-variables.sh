8b9b3334aa80a5016487dbebadf58576f997fb0b
Rodrigo Werlang <rodrigo@paradigmabs.com.br>
2024-06-06 16:31:59 -0300

    fix agent PATH variable and other environment variables

--- images/ubuntu/assets/post-gen/environment-variables.sh
+++ images/ubuntu/assets/post-gen/environment-variables.sh
@@ -2,5 +2,13 @@
 
 # Replace $HOME with the default user's home directory for environmental variables related to the default user home directory
 
+# When running with 'sudo su -c ...' the $HOME user is /root, not the VM primary admin user, so we need to find it
+vmAdmin=$(cut -d: -f6 /etc/passwd | tail -2 | head -n 1)
+
 homeDir=$(cut -d: -f6 /etc/passwd | tail -1)
-sed -i "s|\$HOME|$homeDir|g" /etc/environment
+sed -i "s|$HOME|$homeDir|g" /etc/environment
+sed -i "s|$vmAdmin|$homeDir|g" /etc/environment
+
+# Create the agent_env_vars script that will be ran by enableagent.sh during the pipeline agent installation
+cat /etc/environment > /etc/profile.d/agent_env_vars.sh
+chmod +x /etc/profile.d/agent_env_vars.sh
