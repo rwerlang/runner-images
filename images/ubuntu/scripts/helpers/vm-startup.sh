7b912cd0d48bf36c7e8cea98908571714a53da1a
Rodrigo Werlang <rodrigo@paradigmabs.com.br>
2024-06-06 18:15:54 -0300

    change vm-startup script

--- images/ubuntu/scripts/helpers/vm-startup.sh
+++ images/ubuntu/scripts/helpers/vm-startup.sh
@@ -1,8 +1,12 @@
 #!/bin/bash -e
 
-# Create the agent_env_vars script that will be ran by enableagent.sh during the pipeline agent installation
-cat /etc/environment > /etc/profile.d/agent_env_vars.sh
-chmod +x /etc/profile.d/agent_env_vars.sh
-
 # Execute all post-generation scripts
 find /opt/post-generation -mindepth 1 -maxdepth 1 -type f -name *.sh -exec bash {} \;
+
+
+# Create the agent_env_vars script that will be ran by enableagent.sh during the pipeline agent installation
+
+echo "#!/bin/bash" > /etc/profile.d/agent_env_vars.sh
+echo 'log_message "Running agent_env_vars.sh"' >> /etc/profile.d/agent_env_vars.sh
+cat /etc/environment >> /etc/profile.d/agent_env_vars.sh
+chmod +x /etc/profile.d/agent_env_vars.sh
